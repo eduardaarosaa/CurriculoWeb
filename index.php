7bce4d78277aabc07b80c6d089b64275893934fc
Duda <eduardacirina@gmail.com>
2019-06-20 14:43:59 -0300

    Pronto

---
+++ index.php
@@ -0,0 +1,127 @@
+<html>
+<head>
+<title>TecEdu</title>
+<link rel="icon" href="logo.png">
+<link rel="stylesheet" type="text/css" href="index.css"/>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
+<body>
+<div id="barra">
+<div id="logo">
+<img class="logo" src='logo.png' width="150px">
+</div>
+<div id="campo">
+<input type="text" class="buscar">
+<button type="submit" class="busca">Pesquisar</button>
+</div>
+<nav id="menu">
+<ul>
+<li><a href ="#">Cursos</li></a>
+<li><a href ="#">Quem Somos</li></a>
+<li><a href ="#">Professores</li></a>
+<li><a href ="#">Localização</li></a>
+<li><a href ="#">Contato</li></a>
+<li><a href ="#">Login</li></a>
+<li><a  class="cadastro" href ="#">Cadastre-se</li></a>
+</ul>
+</nav>
+</div>
+<div id="banner">
+<img src='banner.jpg' width='100%'>
+</div>
+<br><br>
+<div class="container">
+  <div class="row">
+  <div class="col-md-4">
+<div class="card" style="width: 18rem;" >
+  <img class="card-img-top" src="php.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Curso de PHP</h5>
+    <p class="card-text">Aprenda a programar em PHP utilizando as melhores práticas e frameworks do mercado. Desde o básico ao avançado, você irá ganhar no mercado.</p>
+    <a href="#" class="btn btn-primary">Veja mais</a>
+  </div>
+  </div>
+  </div>
+<div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="mysql.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Curso de Mysql</h5>
+    <p class="card-text">O curso MySQL - Módulo I apresenta as técnicas básicas de utilização da linguagem SQL no gerenciador de banco de dados MySQL.</p>
+    <a href="#" class="btn btn-primary">Veja mais</a>
+  </div>
+  </div>
+  </div>
+   <div class="col-md-4">
+   <div class="row">
+<div class="card" style="width: 18rem;" class='col-md-4'>
+  <img class="card-img-top" src="js.jpeg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Curso de Front End</h5>
+    <p class="card-text">Curso completo para quem quer aprender a criar sites utilizando as tecnologias de HTML5 + CSS3 + JavaScript de uma maneira simples e objetiva.</p>
+    <a href="#" class="btn btn-primary">Veja mais</a>
+  </div>
+  </div>
+  </div>
+</div>
+</div>
+</div>
+<br>
+<div class="container">
+  <div class="row">
+  <div class="col-md-4">
+<div class="card" style="width: 18rem;" >
+  <img class="card-img-top" src="bts.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Curso de Bootstrap</h5>
+    <p class="card-text">
+Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web usando HTML, CSS e JavaScript.</p>
+    <a href="#" class="btn btn-primary">Veja mais</a>
+  </div>
+  </div>
+  </div>
+<div class="col-md-4">
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="java.jpg" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Curso de Java</h5>
+    <p class="card-text">
+Aprenda a utilizar a linguagem Java no desenvolvimento de aplicações, conhecendo as características e recursos da versão 8 da plataforma Java, utilizando estruturas  e usando NetBeains.
+	</p>
+    <a href="#" class="btn btn-primary">Veja mais</a>
+  </div>
+  </div>
+  </div>
+   <div class="col-md-4">
+   <div class="row">
+<div class="card" style="width: 18rem;" class='col-md-4'>
+  <img class="card-img-top" src="angular.png" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Curso de Angular</h5>
+    <p class="card-text">
+	Angular é um framework que permite desenvolver aplicações web e mobile, mantido pela Google. Apesar de ser a segunda versão do framework, Angular não é a continuação do Angular v1.Venha Aprender!
+	</p>
+    <a href="#" class="btn btn-primary">Veja mais</a>
+  </div>
+  </div>
+  </div>
+</div>
+</div>
+</div>
+<br>
+<br>
+<footer>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.0656932253646!2d-46.65298398544233!3d-23.566083984680514!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59c8da0aa315%3A0xd59f9431f2c9776a!2sAv.+Paulista+-+Bela+Vista%2C+S%C3%A3o+Paulo+-+SP!5e0!3m2!1spt-BR!2sbr!4v1532986432555" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+<ul class='rodape'>
+<li><a href ="#">Cursos</li></a>
+<li><a href ="#">Quem Somos</li></a>
+<li><a href ="#">Professores</li></a>
+<li><a href ="#">Localização</li></a>
+<li><a href ="#">Contato</li></a>
+<li><a href ="#">Login</li></a>
+<li><a href ="#">Cadastre-se</li></a>
+</ul>
+</footer>
+</body>
+</html>
